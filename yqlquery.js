699a3e600b622bc32c3a7beb1aa9ff9edc839703
Adrian Statescu <mergesortv@gmail.com>
2010-07-05 22:20:20 -0700

    first commit added files

---
+++ yqlquery.js
@@ -0,0 +1,27 @@
+//let's build following constructor for YQL
+function YQLQuery(query,callback) {
+     this.query = query;
+     this.callback = callback || function(){};
+     this.fetch = function() {
+           if(!this.query || !this.callback) {
+                    throw new Error("YQLQuery.fetch(): Parameters may be undefined!"); 
+           } 
+
+           var scriptEl = document.createElement('script'),
+               uid = 'yql'+ +new Date(),
+               encodedQuery = encodeURIComponent(this.query),
+               instance = this;
+
+            YQLQuery[uid] = function(json) {
+               instance.callback(json);
+               delete YQLQuery[uid];
+               document.body.removeChild(scriptEl);  
+            };
+
+            scriptEl.src = 'http://query.yahooapis.com/v1/public/yql?q='+
+                            encodedQuery + '&format=json&callback=YQLQuery.' + uid+
+                           '&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys';
+            if(window.console){console.log(decodeURIComponent(encodedQuery));}
+            document.body.appendChild(scriptEl);         
+     };          
+};
